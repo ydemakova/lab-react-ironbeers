2173840a3245fd6a4a24c768baef5c4f53481037
Yulia Demakova <j.demakova@gmail.com>
2021-12-05 18:49:44 +0100

    implment subpages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import AllBeers from './components/AllBeers';
+import RandomBeer from './components/RandomBeer';
+import NewBeer from './components/NewBeer';
 
 function App() {
   return (
     <main>
       <Routes>
         <Route path="/" element={<Home />} exact></Route>
-        <Route path="/beers" element={<Home />} exact></Route>
-        <Route path="/random-beer" element={<Home />} exact></Route>
-        <Route path="/new-beer" element={<Home />} exact></Route>
+        <Route path="/beers" element={<AllBeers />} exact></Route>
+        <Route path="/random-beer" element={<RandomBeer />} exact></Route>
+        <Route path="/new-beer" element={<NewBeer />} exact></Route>
       </Routes>
     </main>
   );
---
+++ src/components/AllBeers.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AllBeers() {
+  return (
+    <div>
+      <h1>All Beers</h1>
+    </div>
+  );
+}
